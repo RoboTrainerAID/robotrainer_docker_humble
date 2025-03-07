f9d46c14149d74906678886af29df773b32b8155
Marie Schelkle <scma1260@h-ka.de>
2025-03-07 14:10:04 +0100

    autostart für  ohne batterie level

--- autostart.sh
+++ autostart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./start_docker.sh "/bin/bash -c 'sudo service dbus start && sudo bluetoothd & sleep 10 && ros2 launch polar_oh1 ros2-polar_oh1.launch.py'"
+./start_docker.sh "/bin/bash -c 'ros2 launch polar_oh1 ros2-polar_oh1.launch.py'"
