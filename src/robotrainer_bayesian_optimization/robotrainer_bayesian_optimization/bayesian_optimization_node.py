cac35aa216118ac0801d488b0cd64a0853ca3b84
Yuliia Cormier <yantsur@gmail.com>
2025-10-10 16:06:24 +0200

    added ground truth mode to bayesian optimization node

--- src/robotrainer_bayesian_optimization/robotrainer_bayesian_optimization/bayesian_optimization_node.py
+++ src/robotrainer_bayesian_optimization/robotrainer_bayesian_optimization/bayesian_optimization_node.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import rclpy
 import yaml
@@ -16,7 +17,9 @@ from .utils import BagReader, create_new_scenario, get_mean_values_from_bag
 
 class BayesianOptimizationNode(Node):
 
-    def __init__(self):
+    def __init__(self, is_ground_truth=False):
+
+        self.is_ground_truth = is_ground_truth
         super().__init__("robotrainer_bayesian_optimization")
         
         self.srv = self.create_service(
@@ -29,86 +32,108 @@ class BayesianOptimizationNode(Node):
             self.study_status_callback,
             10,
         )
-
         self.study_status = ""
-        self.bags_folder = "/home/docker/ros_ws/data/bags/"
-        # Load initial data from config file
-        self.config_file_path = "/home/docker/ros_ws/src/robotrainer_bayesian_optimization/robotrainer_bayesian_optimization/config.yaml"
-        self.initial_data = {}
-        with open(self.config_file_path, "r") as file:
-            config = yaml.safe_load(file)
-            self.initial_data = config["initial_data_files"]
 
-        self.preexisting_trials = []
-        
-        self.bag_reader = BagReader()
-        
-        for key, value in self.initial_data.items():
-            self.get_logger().info(f"Loaded initial data: {key} -> {value}")
-            mean_values = get_mean_values_from_bag(value, self.bag_reader)
-            trial_data = (
-                {"virtual_force": key},
-                {"wrench_force": mean_values["wrench_force_y"]},
+        if not self.is_ground_truth:
+            self.bags_folder = "/home/docker/ros_ws/data/bags/"
+            # Load initial data from config file
+            self.config_file_path = "/home/docker/ros_ws/src/robotrainer_bayesian_optimization/robotrainer_bayesian_optimization/config.yaml"
+            self.initial_data = {}
+            with open(self.config_file_path, "r") as file:
+                config = yaml.safe_load(file)
+                self.initial_data = config["initial_data_files"]
+
+            self.preexisting_trials = []
+            
+            self.bag_reader = BagReader()
+            
+            for key, value in self.initial_data.items():
+                self.get_logger().info(f"Loaded initial data: {key} -> {value}")
+                mean_values = get_mean_values_from_bag(value, self.bag_reader)
+                trial_data = (
+                    {"virtual_force": key},
+                    {"wrench_force": mean_values["wrench_force_y"]},
+                )
+                self.preexisting_trials.append(trial_data)
+
+            self.get_logger().info(f"Loaded initial values: {self.preexisting_trials}")
+
+            # Init BO model
+            generation_strategy = construct_generation_strategy(
+                generator_spec=GeneratorSpec(
+                    model_enum=Generators.BOTORCH_MODULAR,
+                    model_kwargs={"botorch_acqf_class": qUpperConfidenceBound},
+                ),
+                node_name="BoTorch w/ Custom Components",
             )
-            self.preexisting_trials.append(trial_data)
 
-        self.get_logger().info(f"Loaded initial values: {self.preexisting_trials}")
+            self.client = Client()
 
-        # Init BO model
-        generation_strategy = construct_generation_strategy(
-            generator_spec=GeneratorSpec(
-                model_enum=Generators.BOTORCH_MODULAR,
-                model_kwargs={"botorch_acqf_class": qUpperConfidenceBound},
-            ),
-            node_name="BoTorch w/ Custom Components",
-        )
+            virtual_force = RangeParameterConfig(
+                name="virtual_force", parameter_type="int", bounds=(0, 120)
+            )
+            parameters = [virtual_force]
+            self.client.configure_experiment(
+                parameters=parameters,
+                # The following arguments are only necessary when saving to the DB
+                name="virtual_force_impact_experimant",
+                description="Find out the impact of virtual force on human participant",
+                owner="developer",
+            )
 
-        self.client = Client()
+            first_metric_name = (
+                "wrench_force"  # this name is used during the optimization loop
+            )
+            objective = (
+                f"{first_metric_name}"  # minimization is specified by the negative sign
+            )
+            self.client.configure_optimization(objective=objective)
+            self.client.set_generation_strategy(
+                generation_strategy=generation_strategy,
+            )
 
-        virtual_force = RangeParameterConfig(
-            name="virtual_force", parameter_type="int", bounds=(0, 120)
-        )
-        parameters = [virtual_force]
-        self.client.configure_experiment(
-            parameters=parameters,
-            # The following arguments are only necessary when saving to the DB
-            name="virtual_force_impact_experimant",
-            description="Find out the impact of virtual force on human participant",
-            owner="developer",
-        )
+            for parameters, data in self.preexisting_trials:
+                trial_index = self.client.attach_trial(parameters=parameters)
+                self.client.complete_trial(trial_index=trial_index, raw_data=data)
 
-        first_metric_name = (
-            "wrench_force"  # this name is used during the optimization loop
-        )
-        objective = (
-            f"{first_metric_name}"  # minimization is specified by the negative sign
-        )
-        self.client.configure_optimization(objective=objective)
-        self.client.set_generation_strategy(
-            generation_strategy=generation_strategy,
-        )
+            next_trial = self.client.get_next_trials(max_trials=1)
+            self.next_index, self.next_parameters = next(iter(next_trial.items()))
+            self.get_logger().info(f"Next suggestion:\n - index: {self.next_index}, \n - parameters: {self.next_parameters}")
+            
+            # Create Scenario
+            new_scenario = create_new_scenario(self.next_parameters["virtual_force"], "initial_scenario")
 
-        for parameters, data in self.preexisting_trials:
-            trial_index = self.client.attach_trial(parameters=parameters)
-            self.client.complete_trial(trial_index=trial_index, raw_data=data)
+            # Save yaml
+            scenario_name = f"initial_scenario.yaml"
+            scenario_path_in_container = (
+                "/home/docker/ros_ws/data/scenarios/" + scenario_name
+            )
+            with open(scenario_path_in_container, "w") as file:
+                yaml.dump(new_scenario, file)
+            self.get_logger().info(f"Created new scenario file: {scenario_name}")
 
-        next_trial = self.client.get_next_trials(max_trials=1)
-        self.next_index, self.next_parameters = next(iter(next_trial.items()))
-        self.get_logger().info(f"Next suggestion:\n - index: {self.next_index}, \n - parameters: {self.next_parameters}")
+            self.get_logger().info("Successfully started robotrainer_bayesian_optimization")
         
-        # Create Scenario
-        new_scenario = create_new_scenario(self.next_parameters["virtual_force"], "initial_scenario")
+        else:
+            self.get_logger().info("Ground truth mode: No Bayesian Optimization, only scenario creation")
+            random.seed(32)
+            self.random_trial_indices = random.sample(range(0, 101, 5), k=21)  # 0,5,10,...,100
+            self.next_index = 0
+            self.next_parameters = {"virtual_force": self.random_trial_indices.pop(0)}
+            
+            # Create Scenario
+            new_scenario = create_new_scenario(self.next_parameters["virtual_force"], "initial_scenario")
 
-        # Save yaml
-        scenario_name = f"initial_scenario.yaml"
-        scenario_path_in_container = (
-            "/home/docker/ros_ws/data/scenarios/" + scenario_name
-        )
-        with open(scenario_path_in_container, "w") as file:
-            yaml.dump(new_scenario, file)
-        self.get_logger().info(f"Created new scenario file: {scenario_name}")
+            # Save yaml
+            scenario_name = f"initial_scenario.yaml"
+            scenario_path_in_container = (
+                "/home/docker/ros_ws/data/scenarios/" + scenario_name
+            )
+            with open(scenario_path_in_container, "w") as file:
+                yaml.dump(new_scenario, file)
+            self.get_logger().info(f"Created new scenario file: {scenario_name}")
 
-        self.get_logger().info("Successfully started robotrainer_bayesian_optimization")
+            self.get_logger().info("Successfully started robotrainer_bayesian_optimization in ground truth mode") 
 
     def study_status_callback(self, msg):
         if not (self.study_status == msg.data):
@@ -125,45 +150,69 @@ class BayesianOptimizationNode(Node):
             response.message = "Can not find bag file path or there are two of them!"
             return response
         self.get_logger().info(f"Bag file path: {bag_file_path}")
+        if not self.is_ground_truth:
+            # Process Data
+            mean_values = get_mean_values_from_bag(bag_file_path, self.bag_reader)
 
-        # Process Data
-        mean_values = get_mean_values_from_bag(bag_file_path, self.bag_reader)
-
-        # Update BO model
-        parameters, raw_data = (
-                self.next_parameters,
-                {"wrench_force": mean_values["wrench_force_y"]},
+            # Update BO model
+            parameters, raw_data = (
+                    self.next_parameters,
+                    {"wrench_force": mean_values["wrench_force_y"]},
+                )
+            self.client.complete_trial(trial_index=self.next_index, raw_data=raw_data)
+            self.get_logger().info(
+                f"Completed trial {self.next_index} with parameters {parameters} and data {raw_data}"
             )
-        self.client.complete_trial(trial_index=self.next_index, raw_data=raw_data)
-        self.get_logger().info(
-            f"Completed trial {self.next_index} with parameters {parameters} and data {raw_data}"
-        )
-        trials = self.client.get_next_trials(max_trials=1)
-        self.next_index, self.next_parameters = next(iter(trials.items()))
-        self.get_logger().info(f"Next suggestion:\n - index: {self.next_index}, \n - parameters: {self.next_parameters}")
-        
-        # Create Scenario
-        scenario_id = f"trial_{self.next_index}_force_{self.next_parameters['virtual_force']}"
-        new_scenario = create_new_scenario(self.next_parameters["virtual_force"], scenario_id)
+            trials = self.client.get_next_trials(max_trials=1)
+            self.next_index, self.next_parameters = next(iter(trials.items()))
+            self.get_logger().info(f"Next suggestion:\n - index: {self.next_index}, \n - parameters: {self.next_parameters}")
+            
+            # Create Scenario
+            scenario_id = f"trial_{self.next_index}_force_{self.next_parameters['virtual_force']}"
+            new_scenario = create_new_scenario(self.next_parameters["virtual_force"], scenario_id)
 
-        # Save yaml
-        scenario_name = f"{scenario_id}.yaml"
-        scenario_path_in_container = (
-            "/home/docker/ros_ws/data/scenarios/" + scenario_name
-        )
-        with open(scenario_path_in_container, "w") as file:
-            yaml.dump(new_scenario, file)
+            # Save yaml
+            scenario_name = f"{scenario_id}.yaml"
+            scenario_path_in_container = (
+                "/home/docker/ros_ws/data/scenarios/" + scenario_name
+            )
+            with open(scenario_path_in_container, "w") as file:
+                yaml.dump(new_scenario, file)
+            
+            response.success = True
+            response.message = scenario_id
+            self.get_logger().info(f"Created new scenario file: {scenario_name}")
+        else:   
+            if not self.random_trial_indices:
+                response.success = False
+                response.message = "All ground truth trials completed!"
+                self.get_logger().info("All ground truth trials completed!")
+                return response
+            # Create Scenario
+            self.next_index += 1
+            self.next_parameters = {"virtual_force": self.random_trial_indices.pop(0)}
+            scenario_id = f"ground_truth_trial_{self.next_index}_force_{self.next_parameters['virtual_force']}"
+            new_scenario = create_new_scenario(self.next_parameters["virtual_force"], scenario_id)
+
+            # Save yaml
+            scenario_name = f"{scenario_id}.yaml"
+            scenario_path_in_container = (
+                "/home/docker/ros_ws/data/scenarios/" + scenario_name
+            )
+            with open(scenario_path_in_container, "w") as file:
+                yaml.dump(new_scenario, file)
+            
+            response.success = True
+            response.message = scenario_id
+            self.get_logger().info(f"Created new scenario file: {scenario_name}")
         
-        response.success = True
-        response.message = scenario_id
-        self.get_logger().info(f"Created new scenario file: {scenario_name}")
         return response
 
 
 def main():
     rclpy.init()
 
-    node = BayesianOptimizationNode()
+    node = BayesianOptimizationNode(is_ground_truth=False)
 
     try:
         rclpy.spin(node)
